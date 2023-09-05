39717bc5285da02b9504c356fd3759c5a6f852cb
Jonathan <chipjc94@gmail.com>
2023-09-05 16:53:06 -0500

    Started part 2, imported dependencies/database setup

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -1,30 +1,41 @@
 # Import the dependencies.
-
-
+from flask import Flask, jsonify
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import numpy as np
+import datetime as dt
 
 #################################################
 # Database Setup
 #################################################
 
+# create engine to hawaii.sqlite
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 # reflect an existing database into a new model
+Base = automap_base()
 
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+measurement = Base.classes.measurement
+station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
 
-
-
+app = Flask(__name__)
 
 #################################################
 # Flask Routes
 #################################################
+
+@app.route('/')
+def home():
+    
